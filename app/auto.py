8ee529dcaaaa150bd4ff486816e6b7da696026cd
Ivan Bogomolov <qhomop@gmail.com>
2023-01-08 09:13:42 +0300

    add page scrolling and delete infinity advertisement scrolling::auto.ru

--- app/auto.py
+++ app/auto.py
@@ -8,46 +8,59 @@ from parser import Parser, basedir
 class AutoParser(Parser):
     def __init__(self):
         Parser.__init__(self, service="auto", debug=True)
-        self.page = 1
         self.price = 85000
-        self.url = f'https://auto.ru/moskva/cars/all/?top_days=2&price_to={self.price}&with_discount=false&sort=cr_date-desc&damage_group=ANY&page={self.page}'
+        self.advertisement = dict()
+        self.url = f'https://auto.ru/moskva/cars/all/?top_days=2&price_to={self.price}&with_discount=false&sort=cr_date-desc&damage_group=ANY&page='
 
     def parse_page(self):
         try:
-            self.load_cookie(url=self.url)
-            href = self.driver.find_elements(by=By.XPATH, value='//a[@class="Link ListingItemTitle__link"]')
-            price = self.driver.find_elements(by=By.XPATH, value='//div[@class="ListingItemPrice__content"]')
-            tech_details = self.driver.find_elements(by=By.XPATH, value='//div[@class="ListingItemTechSummaryDesktop ListingItem__techSummary"]')
-            mileage = self.driver.find_elements(by=By.XPATH, value='//div[@class="ListingItem__kmAge"]')
-            year = self.driver.find_elements(by=By.XPATH, value='//div[@class="ListingItem__yearBlock"]')
+            page = 0
+            while True:
+                page += 1
+                print(page)
+                if page == 1:
+                    self.load_cookie(url=self.url+str(page))
+                else:
+                    self.driver.get(url=self.url+str(page))
 
-            ad_href = list(map(lambda x: x.get_attribute('href'), href))
-            ad_name = list(map(lambda x: x.text, href))
-            ad_price = list(map(lambda x: x.text, price))
-            ad_details = list(map(lambda x: x.text, tech_details))
-            ad_milage = list(map(lambda x: x.text, mileage))
-            ad_year = list(map(lambda x: x.text, year))
+                # delete infinity advertisement scrolling
+                self.driver.execute_script('const remove = (sel) => document.querySelectorAll(sel).forEach(el => el.remove()); remove(".ListingInfiniteDesktop__snippet");')
 
-            advertisement = dict()
+                href = self.driver.find_elements(by=By.XPATH, value='//a[@class="Link ListingItemTitle__link"]')
+                if len(href) != 0:
+                    price = self.driver.find_elements(by=By.XPATH, value='//div[@class="ListingItemPrice__content"]')
+                    tech_details = self.driver.find_elements(by=By.XPATH, value='//div[@class="ListingItemTechSummaryDesktop ListingItem__techSummary"]')
+                    mileage = self.driver.find_elements(by=By.XPATH, value='//div[@class="ListingItem__kmAge"]')
+                    year = self.driver.find_elements(by=By.XPATH, value='//div[@class="ListingItem__yearBlock"]')
 
-            for i in range(len(ad_href)):
-                advertisement[ad_href[i]] = {
-                    "href": ad_href[i],
-                    "name": ad_name[i],
-                    "price": ad_price[i],
-                    "details": ad_details[i],
-                    "milage": ad_milage[i],
-                    "year": ad_year[i],
-                }
+                    ad_href = list(map(lambda x: x.get_attribute('href'), href))
+                    ad_name = list(map(lambda x: x.text, href))
+                    ad_price = list(map(lambda x: x.text, price))
+                    ad_details = list(map(lambda x: x.text, tech_details))
+                    ad_milage = list(map(lambda x: x.text, mileage))
+                    ad_year = list(map(lambda x: x.text, year))
 
-            print(advertisement)
+                    for i in range(len(ad_href)):
+                        self.advertisement[ad_href[i]] = {
+                            "href": ad_href[i],
+                            "name": ad_name[i],
+                            "price": ad_price[i],
+                            "details": ad_details[i],
+                            "milage": ad_milage[i],
+                            "year": ad_year[i],
+                        }
+                else:
+                    break
+
+            print(self.advertisement)
+
 
         except Exception as e:
             return e
 
-        finally:
-            self.close_parser()
+        # finally:
+        #     self.close_parser()
 
 ap = AutoParser()
-
 ap.parse_page()
+print(ap.advertisement)
